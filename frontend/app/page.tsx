f0a20592eef520c46fd77522c1fe909ccd9642e1
Matheus Kenzo <100180936+matheuskya@users.noreply.github.com>
2026-02-17 00:12:52 -0300

    added check function

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,17 +1,35 @@
+"use client";
 import Form from 'next/form'
+import hello from './hello/page';
 
 export default function home() {
+    function helloWorld() {
+        console.log("hello world")
+    }
+    async function sendGetCheck() {
+        console.log("function call")
+        try {
+            const res = await fetch("http://localhost8080/ping");
+            const data = await res.json();
+            console.log(data)
+        } catch (e) {
+            console.log(e);
+            console.log("Error during check");
+        }
+    }
     return (
-        <Form action={''}>
+        <form>
             <label>Username:</label>
             <input id="username" name="username"></input>
             <br></br>
             <label>Password:</label>
-            <input id="password"></input>
+            <input id="password" name="password"></input>
 
             <br></br>
 
             <button type="submit">Submit</button>
-        </Form>
+            <br></br>
+            <button type="button" onClick={sendGetCheck}> Check connection</button>
+        </form>
     )
 }
