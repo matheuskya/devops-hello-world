ba694bec7754991329884aafecf5a5f5d5068a81
Matheus Kenzo <100180936+matheuskya@users.noreply.github.com>
2026-03-02 20:07:56 -0300

    updated controller

---
+++ demo/src/main/java/com/matheuskya/demo/users/UsersController.java
@@ -0,0 +1,30 @@
+package com.matheuskya.demo.users;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsersController {
+
+    @GetMapping("/checkDb")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @GetMapping("ping")
+    public Map<String, String> ping() {
+        return Map.of("message", "pong");
+    }
+
+    @PostMapping("login")
+    public Map<String, String> login(@RequestBody Map<String, String> request) {
+        System.out.println("REQUEST PARAM");
+        System.out.println(request.get("username"));
+        System.out.println(request.get("password"));
+        return Map.of("user_from_spring", request.get("username"));
+    }
+}
