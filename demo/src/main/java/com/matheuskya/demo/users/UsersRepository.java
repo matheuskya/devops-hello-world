1819d4f63297be038aa09d4dc3028e4c3f7367f2
Matheus Kenzo <100180936+matheuskya@users.noreply.github.com>
2026-03-02 20:08:21 -0300

    added repository

---
+++ demo/src/main/java/com/matheuskya/demo/users/UsersRepository.java
@@ -0,0 +1,9 @@
+package com.matheuskya.demo.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Long> {
+
+}
